e328d056472e968465371abe1851d5d2c8f29a75
De la fuente <delafuentecindy@gmail.com>
2019-06-06 08:29:36 +0200

    build

--- src/server.js
+++ src/server.js
@@ -7,8 +7,12 @@ app.get("/", function(req, res) {
   res.send(data);
 });
 const PORT = process.env.PORT || 3000;
+console.log(`==============> ${process.env.PORT}`);
 try {
-  app.listen(PORT, function() {
+  app.listen(PORT, function(err) {
+    if (err) {
+      console.log(err);
+    }
     console.log("Example app listening on port 3000!");
   });
 } catch (err) {
