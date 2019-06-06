3d4557cbf3b33dd14b7d258e752add025f8f3e16
De la fuente <delafuentecindy@gmail.com>
2019-06-06 08:08:19 +0200

    build

---
+++ build/server.js
@@ -0,0 +1,22 @@
+"use strict";
+
+var _express = require("express");
+
+var _express2 = _interopRequireDefault(_express);
+
+var _data = require("../data.json");
+
+var _data2 = _interopRequireDefault(_data);
+
+function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
+
+var app = (0, _express2.default)();
+
+app.get("/", function (req, res) {
+  res.send(_data2.default);
+});
+var PORT = process.env.PORT || 3000;
+app.listen(PORT, function () {
+  console.log("Example app listening on port 3000!");
+});
+//# sourceMappingURL=server.js.map
--- src/server.js
+++ src/server.js
@@ -1,5 +1,5 @@
 import express from "express";
-import data from "./data.json";
+import data from "../data.json";
 
 const app = express();
 
